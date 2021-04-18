b13882c0f774aefe16edcf42394b0767a3dbd6e8
hm <hm_huangmeng@sina.cn>
2021-04-19 00:08:47 +0800

    week03

---
+++ Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # 递归
+        if p == root or q == root:
+            return root
+        
+        left = None
+        right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        else:
+            return left or right
